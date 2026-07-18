ebebf5aa133befb3c8139a70cfeb57e6b51aa4db
Damini Gidwani <daminigidwani13@gmail.com>
2026-07-18 20:44:31 +0530

    update css

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
+// import './App.css'
 import Navbar from './Portfolio/Navbar/Navbar.jsx'
 import './Portfolio/Navbar/Navbar.css'
 
